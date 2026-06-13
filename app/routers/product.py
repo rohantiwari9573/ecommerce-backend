2f6a91c424058cab1245daf053b3b6ca2e164062
Rohan <rohantiwari166@gmail.com>
2026-06-14 02:25:00 +0530

    Temporarily removed Redis caching for Render

--- app/routers/product.py
+++ app/routers/product.py
@@ -3,8 +3,6 @@ from sqlalchemy.orm import Session
 from app.models.product import Product
 from app.schemas.product import ProductCreate
 from app.dependencies import get_db
-from app.core.redis import redis_client
-import json
 
 router = APIRouter()
 
@@ -15,33 +13,23 @@ def create_product(product: ProductCreate, db: Session = Depends(get_db)):
         name=product.name,
         price=product.price
     )
+
     db.add(new_product)
     db.commit()
     db.refresh(new_product)
-
-    # ❗ invalidate cache
-    redis_client.delete("products")
 
     return new_product
 
 
 @router.get("/products")
 def get_products(db: Session = Depends(get_db)):
-    # 🔥 CHECK CACHE
-    cached_products = redis_client.get("products")
-
-    if cached_products:
-        return json.loads(cached_products)
-
-    # ❌ CACHE MISS → DB HIT
     products = db.query(Product).all()
 
-    data = [
-        {"id": p.id, "name": p.name, "price": p.price}
+    return [
+        {
+            "id": p.id,
+            "name": p.name,
+            "price": p.price
+        }
         for p in products
     ]
-
-    # 🔥 STORE IN REDIS (TTL 60 sec)
-    redis_client.setex("products", 60, json.dumps(data))
-
-    return data
