d86b46f9804c95a63d6168654fb047bd8256c1ff
Rohan <rohantiwari166@gmail.com>
2026-06-14 01:47:21 +0530

    Fixed main.py imports and routers

--- app/main.py
+++ app/main.py
@@ -6,21 +6,21 @@ from app.database import engine, Base
 from app.models import user, product, cart, order
 
 # Import routers
-from app.routes import user_routes
-from app.routes import product_routes
-from app.routes import cart_routes
-from app.routes import order_routes
+from app.routers import user
+from app.routers import product
+from app.routers import cart
+from app.routers import order
 
-# Create DB tables
+# Create tables
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
 # Include routers
-app.include_router(user_routes.router)
-app.include_router(product_routes.router)
-app.include_router(cart_routes.router)
-app.include_router(order_routes.router)
+app.include_router(user.router)
+app.include_router(product.router)
+app.include_router(cart.router)
+app.include_router(order.router)
 
 
 @app.get("/")
